a385cc45c69933a7d019483d6d25b5daeae73375
Ludvig Svedberg <ludvig.svedberg@mac.com>
2021-12-15 12:34:22 +0100

    Remove console log of process.env

--- src/api/firebase/config.ts
+++ src/api/firebase/config.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
-console.log(process.env)
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_apiKey,
     authDomain: process.env.REACT_APP_authDomain,
