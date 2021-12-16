bfeb5411d038c1ccf938181b496cbac0b24cf5cc
Ludvig Svedberg <ludvig.svedberg@mac.com>
2021-12-16 21:31:57 +0100

    Update workflow with api key and move non-secret environment variables into config

--- src/api/firebase/config.ts
+++ src/api/firebase/config.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { initializeFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId,
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: 'lokalvardare-54743.firebaseapp.com',
+    projectId: 'lokalvardare-54743',
+    storageBucket: 'lokalvardare-54743.appspot.com',
+    messagingSenderId: '355231012187',
+    appId: '1:355231012187:web:83ade7eefc282ec190f67e',
 }
 
 const app = initializeApp(firebaseConfig)
